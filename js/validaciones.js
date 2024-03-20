ed9a3aad4ed88f6824bf3a5511857fc73128bfa4
Nicolas <nicoeei@gmail.com>
2024-03-20 13:28:40 -0300

    Arreglo de validaciones y manda mail

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,61 +1,40 @@
+
 const form = document.getElementById("form");
 const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const email = document.getElementById("email");
 const mensaje = document.getElementById("mensaje");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+//form.addEventListener("submit", (e) => {
+  //e.preventDefault();
 
-  checkInputs();
-});
+  //let condicion = checkInputs();
+  //if (condicion) {
+   // enviarFormulario();
+  //}
+//});
 
-function checkInputs() {
-  // trim to remove the whitespaces
-  const nombreValue = nombre.value.trim();
-  const apellidoValue = apellido.value.trim();
-  const emailValue = email.value.trim();
-  const mensajeValue = mensaje.value.trim();
+ document.addEventListener('submit', (e) => {
+        if (!checkInputs()) {
+           e.preventDefault();
+          
+        }
+      });
+      
+    
 
-  if (nombreValue === "") {
-    setErrorFor(nombre, "No puede dejar la casilla nombre en blanco");
-  } else if (!validateName(nombreValue)) {
-    setErrorFor(nombre, "Solo se aceptan letras");
-  } else {
-    setSuccessFor(nombre);
-  }
-
-  if (apellidoValue === "") {
-    setErrorFor(apellido, "No puede dejar la casilla apellido en blanco");
-  } else if (!validateName(apellidoValue)) {
-    setErrorFor(apellido, "Solo se aceptan letras");
-  } else {
-    setSuccessFor(apellido);
-  }
-  if (emailValue === "") {
-    setErrorFor(email, "No puede dejar el email en blanco");
-  } else if (!isEmail(emailValue)) {
-    setErrorFor(email, "No ingreso un email válido");
-  } else {
-    setSuccessFor(email);
-  }
-
-  if (mensajeValue === "") {
-    setErrorFor(mensaje,"No puede dejar el mensaje en blanco");
-  } else {
-    setSuccessFor(mensaje);
-  }
-}
 function setErrorFor(input, message) {
   const inputContainer = input.parentElement;
   const small = inputContainer.querySelector('small');
   inputContainer.className = "inputContainer error";
   small.innerText = message;
+  return false;
 }
 
 function setSuccessFor(input) {
   const inputContainer = input.parentElement;
   inputContainer.className = "inputContainer success";
+  return true;
 }
 
 //Valida que tiene ser un email
@@ -71,3 +50,57 @@ function validateName(nombre, apellido) {
 
   return patt.test(nombre, apellido);
 }
+
+
+function enviarformulario(){
+  form.reset();
+
+}
+
+function checkInputs() {
+  // trim to remove the whitespaces
+   let status = false; 
+  const nombreValue = nombre.value.trim();
+  const apellidoValue = apellido.value.trim();
+  const emailValue = email.value.trim();
+  const mensajeValue = mensaje.value.trim();
+
+  if (nombreValue === "") {
+    status = setErrorFor(nombre, "No puede dejar la casilla nombre en blanco");
+    
+  } else if (!validateName(nombreValue)) {
+    status = setErrorFor(nombre, "Solo se aceptan letras");
+    
+  } else {
+    status = setSuccessFor(nombre);
+  }
+
+  if (apellidoValue === "") {
+   status = setErrorFor(apellido, "No puede dejar la casilla apellido en blanco");
+    
+  } else if (!validateName(apellidoValue)) {
+    status = setErrorFor(apellido, "Solo se aceptan letras");
+     
+  } else {
+    status = setSuccessFor(apellido);
+  }
+  
+  if (emailValue === "") {
+    status = setErrorFor(email, "No puede dejar el email en blanco");
+     
+  } else if (!isEmail(emailValue)) {
+    status = setErrorFor(email, "No ingreso un email válido");
+     
+  } else {
+    status = setSuccessFor(email);
+  }
+
+  if (mensajeValue === "") {
+    status = setErrorFor(mensaje,"No puede dejar el mensaje en blanco");
+     
+  } else {
+    status = setSuccessFor(mensaje);
+  }
+ return status;
+}
+
