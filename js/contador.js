be60352b72ea1df19375f493f0a6015324badf93
Nicolas <nicoeei@gmail.com>
2024-03-24 19:02:14 -0300

    Arregle el contador para que se reproduzca una vez al scrollear

--- js/contador.js
+++ js/contador.js
@@ -1,48 +1,43 @@
-
-$.fn.jQuerySimpleCounter = function (options) {
-  var settings = $.extend(
-    {
-      start: 0,
-      end: 100,
-      easing: "swing",
-      duration: 400,
-      complete: "",
-    },
-    options
-  );
-
-  var thisElement = $(this);
+let section_counter = document.querySelector('#contador');
+let counters = document.querySelectorAll('.contenedorbarra .number');
 
 
-document.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('.contenedorbarra');
+let CounterObserver = new IntersectionObserver(
+  (entries, observer) => {
+    let [entry] = entries;
+    if (!entry.isIntersecting) return;
 
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            section.classList.add('active'); 
-            $({ count: settings.start }).animate(
-    { count: settings.end },
-    {
-      duration: settings.duration,
-      easing: settings.easing,
-      step: function () {
-        var mathCount = Math.ceil(this.count);
-        thisElement.text(mathCount);
-      },
-      complete: settings.complete,
-    }
-  );
-        } else {
-            section.classList.remove('active');
+    let speed = 200;
+    counters.forEach((number, index) => {
+      function UpdateCounter() {
+        const targetNumber = +number.dataset.target;
+        const initialNumber = +number.innerText;
+        const incPerCount = targetNumber / speed;
+        if (initialNumber < targetNumber) {
+          number.innerText = Math.ceil(initialNumber + incPerCount);
+          //Aca se cambia la velocidad
+          setTimeout(UpdateCounter, 20);
         }
+        else {
+          counter.innerText = targetNumber;
+        }
+      }
+      UpdateCounter();
+
+      if (number.parentElement.style.animation) {
+        number.parentElement.style.animation = '';
+      } else {
+        number.parentElement.style.animation = `slide-up 0.3s ease forwards ${
+          index / counters.length + 0.5
+        }s`;
+      }
     });
-});
+    observer.unobserve(section_counter);
+  },
+  {
+    root: null,
+    threshold: window.innerWidth > 768 ? 0.4 : 0.3,
+  }
+);
 
-  
-};
-
-$("#number1").jQuerySimpleCounter({ end: 12, duration: 3000 });
-$("#number2").jQuerySimpleCounter({ end: 55, duration: 3000 });
-$("#number3").jQuerySimpleCounter({ end: 359, duration: 2000 });
-$("#number4").jQuerySimpleCounter({ end: 246, duration: 2500 });
+CounterObserver.observe(section_counter);
